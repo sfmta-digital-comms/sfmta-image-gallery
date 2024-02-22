3ca2652d01ec912d9d543ce1e14f8002eb1bd7b4
Jesse Woltjer <jesse@ruddyduck.mynetworksettings.com>
2024-02-22 12:34:39 -0800

    Added descriptions to protos.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,13 @@ function Home() {
     <div className="home">
       <nav>
         <Link to="/image-gallery">Direction 1: Image Carousel</Link>
+        <p>Better on a desktop. Mobile a bit clunky.</p><br />
         <Link to="/image-grid">Direction 2: Image Grid</Link>
+        <p>It is an "ok" way to show images of different aspect ratios on desktop. Bad on mobile</p><br />
         <Link to="/image-custom-grid">Direction 3: Custom Grid</Link>
+        <p>Thumbnails cropped to squares. Best on mobile</p><br />
         <Link to="/image-custom-grid-no-border">Direction 4: Custom Grid No Border</Link>
+        <p>Thumbnails cropped to squares. No border between thumbnails. Best on mobile</p>
       </nav>
     </div>
   );
